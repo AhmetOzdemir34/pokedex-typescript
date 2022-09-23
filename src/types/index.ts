f69da31d2a5d190568dfc724fbccea5e7c73c38a
Ahmet Özdemir <ahmetzcom.online@gmail.com>
2022-09-23 17:56:42 +0300

    add 2 interfaces for type annotations

---
+++ src/types/index.ts
@@ -0,0 +1,17 @@
+export interface SearchPokemon {
+    name: string,
+    id: number,
+    height: number,
+    weight: number,
+    sprites: any
+}
+
+export interface DetailPokemon {
+    name: string,
+    id: number,
+    height: number,
+    weight: number,
+    sprites: any,
+    moves: any,
+    types: any
+}
