17d76183fa8d554aaaee0d2d0213638cf472e0e0
Ahmet Özdemir <ahmetzcom.online@gmail.com>
2022-09-23 17:57:52 +0300

    configured all routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,12 +11,14 @@ const routes: Array<RouteConfig> = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/favs',
+    name: 'favs',
+    component: () => import('../views/FavsView.vue')
+  },
+  {
+    path: '/pokemon/:name',
+    name: 'pokemon',
+    component: () => import('../views/PokemonView.vue')
   }
 ]
 
