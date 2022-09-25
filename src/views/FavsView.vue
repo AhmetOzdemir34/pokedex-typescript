cf592dfb7911a5ed2800b7ef48b7db4f11af0bbc
Ahmet Özdemir <ahmetzcom.online@gmail.com>
2022-09-25 12:39:10 +0300

    responsive design

--- src/views/FavsView.vue
+++ src/views/FavsView.vue
@@ -1,13 +1,15 @@
 <template>
     <div class="max-w-7xl mx-auto">
         <div v-if="favs.length!==0" class="mt-5">
-            <div class="flex flex-row items-center">
-                <div v-for="(e,i) in favs" :key="i" class="w-10/12 mx-auto sm:w-5/12 md:w-4/12 lg:w-2/12 rounded-t bg-slate-200">
-                    <img v-bind:src="e.sprites.front_default" alt="no image" class="w-[150px] h-[150px] mx-auto block">
-                    <p class="text-center uppercase font-bold mb-2">{{e.name}}</p>
-                    <button type="button" @click="discardItInFavs(e.id)" class="text-center w-full block p-2 bg-red-500 hover:bg-red-400 transition-all duration-300">
-                        <i class="fa-solid fa-trash text-white"></i>
-                    </button>
+            <div class="flex flex-row flex-wrap justify-start items-center">
+                <div v-for="(e,i) in favs" :key="i" class="w-10/12 sm:w-4/12 md:w-3/12 lg:w-2/12 rounded-t p-2">
+                    <div class="p-2 bg-slate-200">
+                        <img v-bind:src="e.sprites.front_default" alt="no image" class="w-[150px] h-[150px] mx-auto block">
+                        <p class="text-center uppercase font-bold mb-2">{{e.name}}</p>
+                        <button type="button" @click="discardItInFavs(e.id)" class="text-center w-full block p-2 bg-red-500 hover:bg-red-400 transition-all duration-300">
+                            <i class="fa-solid fa-trash text-white"></i>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
