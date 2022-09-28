262ba95169bd0b3ebd381b520524e868a8d71dc1
Ahmet Özdemir <ahmetzcom.online@gmail.com>
2022-09-28 14:08:17 +0300

    added pokemon mock data

---
+++ src/mocks/index.ts
@@ -0,0 +1,424 @@
+import { SearchPokemon } from "@/types";
+
+export const pokemonsMock:SearchPokemon[] = [
+    {
+    name: "clefairy",
+    id: 35,
+    height: 6,
+    weight: 75,
+    sprites: {
+        back_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/35.png",
+        back_female:null,
+        back_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/35.png",
+        back_shiny_female:null,
+        front_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/35.png",
+        front_female:null,
+        front_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/35.png",
+        front_shiny_female:null,
+        other:{
+            home:{
+                front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/35.png",
+            }
+        }
+    }
+},
+{
+    name: "bulbasaur",
+    id: 1,
+    height: 7,
+    weight: 69,
+    sprites: {
+        back_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png",
+        back_female:null,
+        back_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/1.png",
+        back_shiny_female:null,
+        front_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+        front_female:null,
+        front_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png",
+        front_shiny_female:null,
+        other:{
+            home:{
+                front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png",
+            }
+        }
+    }
+},
+{
+    name: "clefairy",
+    id: 35,
+    height: 6,
+    weight: 75,
+    sprites: {
+        back_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/35.png",
+        back_female:null,
+        back_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/35.png",
+        back_shiny_female:null,
+        front_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/35.png",
+        front_female:null,
+        front_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/35.png",
+        front_shiny_female:null,
+        other:{
+            home:{
+                front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/35.png",
+            }
+        }
+    }
+},
+{
+    name: "bulbasaur",
+    id: 1,
+    height: 7,
+    weight: 69,
+    sprites: {
+        back_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png",
+        back_female:null,
+        back_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/1.png",
+        back_shiny_female:null,
+        front_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+        front_female:null,
+        front_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png",
+        front_shiny_female:null,
+        other:{
+            home:{
+                front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png",
+            }
+        }
+    }
+},
+{
+    name: "clefairy",
+    id: 35,
+    height: 6,
+    weight: 75,
+    sprites: {
+        back_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/35.png",
+        back_female:null,
+        back_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/35.png",
+        back_shiny_female:null,
+        front_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/35.png",
+        front_female:null,
+        front_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/35.png",
+        front_shiny_female:null,
+        other:{
+            home:{
+                front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/35.png",
+            }
+        }
+    }
+},
+{
+    name: "bulbasaur",
+    id: 1,
+    height: 7,
+    weight: 69,
+    sprites: {
+        back_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png",
+        back_female:null,
+        back_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/1.png",
+        back_shiny_female:null,
+        front_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+        front_female:null,
+        front_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png",
+        front_shiny_female:null,
+        other:{
+            home:{
+                front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png",
+            }
+        }
+    }
+},
+{
+    name: "clefairy",
+    id: 35,
+    height: 6,
+    weight: 75,
+    sprites: {
+        back_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/35.png",
+        back_female:null,
+        back_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/35.png",
+        back_shiny_female:null,
+        front_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/35.png",
+        front_female:null,
+        front_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/35.png",
+        front_shiny_female:null,
+        other:{
+            home:{
+                front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/35.png",
+            }
+        }
+    }
+},
+{
+    name: "bulbasaur",
+    id: 1,
+    height: 7,
+    weight: 69,
+    sprites: {
+        back_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png",
+        back_female:null,
+        back_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/1.png",
+        back_shiny_female:null,
+        front_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+        front_female:null,
+        front_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png",
+        front_shiny_female:null,
+        other:{
+            home:{
+                front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png",
+            }
+        }
+    }
+},
+{
+    name: "clefairy",
+    id: 35,
+    height: 6,
+    weight: 75,
+    sprites: {
+        back_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/35.png",
+        back_female:null,
+        back_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/35.png",
+        back_shiny_female:null,
+        front_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/35.png",
+        front_female:null,
+        front_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/35.png",
+        front_shiny_female:null,
+        other:{
+            home:{
+                front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/35.png",
+            }
+        }
+    }
+},
+{
+    name: "bulbasaur",
+    id: 1,
+    height: 7,
+    weight: 69,
+    sprites: {
+        back_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png",
+        back_female:null,
+        back_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/1.png",
+        back_shiny_female:null,
+        front_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+        front_female:null,
+        front_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png",
+        front_shiny_female:null,
+        other:{
+            home:{
+                front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png",
+            }
+        }
+    }
+},
+{
+    name: "clefairy",
+    id: 35,
+    height: 6,
+    weight: 75,
+    sprites: {
+        back_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/35.png",
+        back_female:null,
+        back_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/35.png",
+        back_shiny_female:null,
+        front_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/35.png",
+        front_female:null,
+        front_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/35.png",
+        front_shiny_female:null,
+        other:{
+            home:{
+                front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/35.png",
+            }
+        }
+    }
+},
+{
+    name: "bulbasaur",
+    id: 1,
+    height: 7,
+    weight: 69,
+    sprites: {
+        back_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png",
+        back_female:null,
+        back_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/1.png",
+        back_shiny_female:null,
+        front_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+        front_female:null,
+        front_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png",
+        front_shiny_female:null,
+        other:{
+            home:{
+                front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png",
+            }
+        }
+    }
+},
+{
+    name: "clefairy",
+    id: 35,
+    height: 6,
+    weight: 75,
+    sprites: {
+        back_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/35.png",
+        back_female:null,
+        back_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/35.png",
+        back_shiny_female:null,
+        front_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/35.png",
+        front_female:null,
+        front_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/35.png",
+        front_shiny_female:null,
+        other:{
+            home:{
+                front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/35.png",
+            }
+        }
+    }
+},
+{
+    name: "bulbasaur",
+    id: 1,
+    height: 7,
+    weight: 69,
+    sprites: {
+        back_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png",
+        back_female:null,
+        back_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/1.png",
+        back_shiny_female:null,
+        front_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+        front_female:null,
+        front_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png",
+        front_shiny_female:null,
+        other:{
+            home:{
+                front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png",
+            }
+        }
+    }
+},
+{
+    name: "clefairy",
+    id: 35,
+    height: 6,
+    weight: 75,
+    sprites: {
+        back_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/35.png",
+        back_female:null,
+        back_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/35.png",
+        back_shiny_female:null,
+        front_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/35.png",
+        front_female:null,
+        front_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/35.png",
+        front_shiny_female:null,
+        other:{
+            home:{
+                front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/35.png",
+            }
+        }
+    }
+},
+{
+    name: "bulbasaur",
+    id: 1,
+    height: 7,
+    weight: 69,
+    sprites: {
+        back_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png",
+        back_female:null,
+        back_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/1.png",
+        back_shiny_female:null,
+        front_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+        front_female:null,
+        front_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png",
+        front_shiny_female:null,
+        other:{
+            home:{
+                front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png",
+            }
+        }
+    }
+},
+{
+    name: "clefairy",
+    id: 35,
+    height: 6,
+    weight: 75,
+    sprites: {
+        back_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/35.png",
+        back_female:null,
+        back_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/35.png",
+        back_shiny_female:null,
+        front_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/35.png",
+        front_female:null,
+        front_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/35.png",
+        front_shiny_female:null,
+        other:{
+            home:{
+                front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/35.png",
+            }
+        }
+    }
+},
+{
+    name: "bulbasaur",
+    id: 1,
+    height: 7,
+    weight: 69,
+    sprites: {
+        back_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png",
+        back_female:null,
+        back_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/1.png",
+        back_shiny_female:null,
+        front_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+        front_female:null,
+        front_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png",
+        front_shiny_female:null,
+        other:{
+            home:{
+                front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png",
+            }
+        }
+    }
+},
+{
+    name: "clefairy",
+    id: 35,
+    height: 6,
+    weight: 75,
+    sprites: {
+        back_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/35.png",
+        back_female:null,
+        back_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/35.png",
+        back_shiny_female:null,
+        front_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/35.png",
+        front_female:null,
+        front_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/35.png",
+        front_shiny_female:null,
+        other:{
+            home:{
+                front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/35.png",
+            }
+        }
+    }
+},
+{
+    name: "bulbasaur",
+    id: 1,
+    height: 7,
+    weight: 69,
+    sprites: {
+        back_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png",
+        back_female:null,
+        back_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/1.png",
+        back_shiny_female:null,
+        front_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+        front_female:null,
+        front_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png",
+        front_shiny_female:null,
+        other:{
+            home:{
+                front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png",
+            }
+        }
+    }
+}
+];
