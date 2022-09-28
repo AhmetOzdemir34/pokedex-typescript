999ec6ca4fcdda7566258d9235a4f729c5053e59
Ahmet Özdemir <ahmetzcom.online@gmail.com>
2022-09-28 14:11:03 +0300

    added unit tests

--- tests/unit/home.spec.ts
+++ tests/unit/home.spec.ts
@@ -1,79 +1,64 @@
 import { SearchPokemon } from "@/types";
-import { mount, shallowMount } from "@vue/test-utils";
+import { createLocalVue, mount, shallowMount } from "@vue/test-utils";
 import axios from "axios";
 import flushPromises from "flush-promises";
-import Vue from "vue";
 import Vuex from "vuex";
 import HomeView from "../../src/views/HomeView.vue";
+import { mutations } from "../../src/store";
+import { pokemonsMock } from "../../src/mocks/index";
+
+const localVue = createLocalVue()
+
+localVue.use(Vuex)
 
 describe('HomeView.vue', ()=>{
-    const pokemonMock:SearchPokemon = {
-        name: "clefairy",
-        id: 35,
-        height: 6,
-        weight: 75,
-        sprites: {
-            back_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/35.png",
-            back_female:null,
-            back_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/35.png",
-            back_shiny_female:null,
-            front_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/35.png",
-            front_female:null,
-            front_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/35.png",
-            front_shiny_female:null
-        }
-    };
 
-    const get = jest.spyOn(axios, 'get');
-    get.mockResolvedValue({data:pokemonMock});
+    const get = jest.spyOn(axios, 'get').mockResolvedValue({data:pokemonsMock});
     
-    const getFailed = jest.spyOn(axios, 'get');
-    getFailed.mockResolvedValue({data:"Not Found"});
-    
-    Vue.use(Vuex);
-    const store = new Vuex.Store({
-        state: {
-            value: "clefairy",
-            pokemons: []
-        }
-    })
-    const store2 = new Vuex.Store({
-        state: {
-            value: "invalid",
-            pokemons: []
-        }
+    let state;
+    let store:any;
+  
+    beforeEach(() => {
+      state = {
+        pokemons: [] as SearchPokemon[],
+        value:'pikachu' as string
+      }
+
+      store = new Vuex.Store({
+        mutations, state
+      })
     })
 
-    it('should mount a page or view', ()=>{
+    it('should mount a page or view', async ()=>{
         const wrapper = mount(HomeView,{
-            store
+            store,localVue
         });
 
         expect(wrapper.exists()).toBe(true);
     });
 
-    it('should include search button', ()=>{
+    it('should mount search button', async ()=>{
         const wrapper = mount(HomeView,{
-            store
+            store,localVue
         });
-        const searchBtn = wrapper.get('[data-test="search"]')
-
-        expect(searchBtn.exists()).toBe(true);
+        expect(wrapper.get('[data-test="search"]').exists()).toBe(true);
     });
 
-    it('should give pokemon data', async ()=>{
-        const wrapper = mount(HomeView,{
-            store:store
-        });
-        await wrapper.get('[data-test="search"]').trigger('click');
-        expect(get).toHaveBeenCalledWith('https://pokeapi.co/api/v2/pokemon/'+store.state.value);
-    })
+    /* it('should mount first 20 pokemon data', async ()=>{
+       const wrapper = mount(HomeView,{
+            store,localVue
+        });        
+        await flushPromises();
+        console.log(wrapper.html());
+        
+        expect(wrapper.findAll('[data-test="init-cards"]').length).toBe(20);
+    }) */
 
-    it('should not give pokemon data with invalid', async ()=>{
+    /* it('should not give pokemon data with invalid', async ()=>{
         const wrapper = mount(HomeView,{
-            store : store2,
+            store,localVue
         });
         await wrapper.get('[data-test="search"]').trigger('click');
         expect(getFailed).toHaveBeenCalledWith('https://pokeapi.co/api/v2/pokemon/'+store.state.value);
-    })
+    }) */
 })
--- tests/unit/pokemon.spec.ts
+++ tests/unit/pokemon.spec.ts
@@ -1,7 +1,9 @@
 import { DetailPokemon } from "@/types";
 import { mount } from "@vue/test-utils";
 import axios from "axios";
+import flushPromises from "flush-promises";
 import PokemonView from "../../src/views/PokemonView.vue";
+
 
 describe('PokemonView.vue', ()=>{
 
@@ -18,7 +20,12 @@ describe('PokemonView.vue', ()=>{
             front_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/35.png",
             front_female:null,
             front_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/35.png",
-            front_shiny_female:null
+            front_shiny_female:null,
+            other:{
+                home:{
+                    front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/35.png",
+                }
+            }
         },
         moves: [
             {
@@ -29,8 +36,42 @@ describe('PokemonView.vue', ()=>{
         ],
     };
 
-    const get = jest.spyOn(axios, 'get');
-    get.mockResolvedValue({data:pokemonMock});
+    jest.spyOn(axios, 'get').mockResolvedValue({data:pokemonMock});
+
+    const localStorageMock = (function () {
+        interface StorageStore{
+          [key:string]: string
+        }
+        let store = {} as StorageStore;
+      
+        return {
+          getItem(key:string) {
+            return store[key];
+          },
+      
+          setItem(key:string, value:string) {
+            store[key] = value;
+          },
+      
+          clear() {
+            store = {};
+          },
+      
+          removeItem(key:string) {
+            delete store[key];
+          },
+      
+          getAll() {
+            return store;
+          },
+        };
+      })();
+      
+      Object.defineProperty(window, "localStorage", { value: localStorageMock });
+    
+      const setLocalStorage = (id:string, data:any) => {
+        window.localStorage.setItem(id, JSON.stringify(data));
+      };
 
     it('should mount a page or view', ()=>{
         const wrapper = mount(PokemonView,{
@@ -40,11 +81,11 @@ describe('PokemonView.vue', ()=>{
                         name:'clefairy'
                     }
                 },
-            },
+            }
         });
+        
         expect(wrapper.exists()).toBe(true);
-    })
-
+    });
     it('Should fetch pokemon data in detail page', async ()=>{
         const wrapper = mount(PokemonView,{
             mocks: {
@@ -53,14 +94,65 @@ describe('PokemonView.vue', ()=>{
                         name:'clefairy'
                     }
                 },
-            },
+            }
         });
-        
-        // basarili api istegi
-        expect(get).toHaveBeenCalledWith('https://pokeapi.co/api/v2/pokemon/clefairy');
+        expect(axios.get).toHaveBeenCalledWith('https://pokeapi.co/api/v2/pokemon/clefairy');
+        /* await flushPromises();
+        console.log(wrapper.html()); */
+
+    });
+    it('Should mount pokemon data', async ()=>{
+        const wrapper = mount(PokemonView,{
+            mocks: {
+                $route: {
+                    params:{
+                        name:'clefairy'
+                    }
+                },
+            }
+        });
+        expect(axios.get).toHaveBeenCalledWith('https://pokeapi.co/api/v2/pokemon/clefairy');
+        await flushPromises();
+        // console.log(wrapper.html());
+        expect(wrapper.get('h2').text()).toContain('clefairy');
+        expect(wrapper.get('[src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/35.png"]').exists()).toBe(true);
+    })
+    it('Add favs button should work', async ()=>{
+        setLocalStorage("pokemons", []);
+        const wrapper = mount(PokemonView,{
+            mocks: {
+                $route: {
+                    params:{
+                        name:'clefairy'
+                    }
+                },
+            }
+        });
+        expect(axios.get).toHaveBeenCalledWith('https://pokeapi.co/api/v2/pokemon/clefairy');
+        await flushPromises();
+        wrapper.get('[data-test="btn-favs-plus"]').trigger('click');
+        await flushPromises();
+        expect(wrapper.get('[data-test="btn-favs-minus"]').exists()).toBe(true);
         
     })
-
+    it('Discard it to Favs button should work', async ()=>{
+        setLocalStorage("pokemons", [{id:35}]);
+        const wrapper = mount(PokemonView,{
+            mocks: {
+                $route: {
+                    params:{
+                        name:'clefairy'
+                    }
+                },
+            }
+        });
+        expect(axios.get).toHaveBeenCalledWith('https://pokeapi.co/api/v2/pokemon/clefairy');
+        await flushPromises();
+        wrapper.get('[data-test="btn-favs-minus"]').trigger('click');
+        await flushPromises();
+        expect(wrapper.get('[data-test="btn-favs-plus"]').exists()).toBe(true);
+        
+    })
     it('Should not fetch pokemon data with invalid params', async ()=>{
         const wrapper = mount(PokemonView,{
             mocks: {
@@ -68,7 +160,7 @@ describe('PokemonView.vue', ()=>{
                     params:{
                         name:'invalid'
                     }
-                }
+                },
             }
         });
 
